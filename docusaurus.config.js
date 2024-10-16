8d2b26664811e5e49435c96b10db65f246c8d9fe
assafsauer <as.sauer@gmail.com>
2024-10-16 09:36:16 +0200

    Update config for correct repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   title: 'STACKTIC',
   tagline: 'Embrace full-stack automation control tactic',
-  url: 'https://www.yourstackticsite.com',
-  baseUrl: '/',
+  url: 'https://stackticio.github.io',  // Your GitHub Pages URL
+  baseUrl: '/documentation/',  // Matches your repository name
   favicon: 'img/favicon.ico',
-  organizationName: 'stacktic',
-  projectName: 'stacktic-website',
+  trailingSlash: false,
+  organizationName: 'stackticio',  // Your GitHub organization/user name
+  projectName: 'documentation',  // Correct GitHub repository name
 
   themeConfig: {
     navbar: {
@@ -13,21 +14,14 @@ module.exports = {
       logo: {
         alt: 'Stacktic',
         src: 'img/transparent_logo_black.png',
-        href: '/docs/overview',  // Redirect to documentation page instead of homepage
+        href: '/docs/overview',  // Redirect to documentation page
         target: '_self',
       },
       items: [
-        // { to: '/', label: 'Home', position: 'right' }, // Home link commented out
         { to: '/docs/overview', label: 'Documentation', position: 'right' },
-        // { to: '/solutions', label: 'Solution', position: 'right' },
-        // { to: '/cases', label: 'Case Studies', position: 'right' },
         { to: '/calculateAllTasksROI', label: 'ROI', position: 'right' },
-        // { to: '/blog', label: 'Blogs', position: 'right' }, // Updated path
-        // { to: '/pricing', label: 'Pricing', position: 'right' },
-        // { to: '/company', label: 'Company', position: 'right' },
       ],
     },
-    // ... other themeConfig settings
   },
 
   presets: [
@@ -36,11 +30,9 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // ... other doc plugin options
         },
         blog: {
           showReadingTime: true,
-          // ... other blog plugin options
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -48,6 +40,7 @@ module.exports = {
       },
     ],
   ],
-  // ... other configuration settings
+
+  deploymentBranch: 'gh-pages',
 };
 
